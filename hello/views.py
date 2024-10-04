eb720fade3312a6c44e975823c3445e9f23b00db
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-04 15:57:04 -0400

    added models

--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import x
 
 def home(request):
-    return HttpResponse("Hello, Django. I am  still still learning Python and Django!")
+    return HttpResponse("Hello, Django. I am  still still learning Python and Django!" + str(x))
 
 
 def about(request):
